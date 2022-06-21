284f1e7d6894a8b1112554bff969353d2e6216d7
guillermo <guiyottemolina@gmail.com>
2022-06-21 19:29:01 -0300

    commit de prueba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Torneo");
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Uno");
+        map.put(3, "Tres");
+        map.put(5, "Cinco");
+        map.put(7, "Siete");
+        map.put(9, "Ocho");
+        System.out.println(map);
     }
 }
+
