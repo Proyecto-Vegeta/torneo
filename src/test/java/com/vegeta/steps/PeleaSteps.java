1a4477a9f8738c898a6076c60cc8c402398ab0e5
LeoTheEvil <iceninjashidomaru@gmail.com>
2023-12-13 15:05:52 -0300

    Test Automation (todavia da null)

--- src/test/java/com/vegeta/steps/PeleaSteps.java
+++ src/test/java/com/vegeta/steps/PeleaSteps.java
@@ -22,7 +22,7 @@ public class PeleaSteps {
     String Ganador;
 
     @Given("$Luchador1_nombre de tipo $Luchador1_tipo con estadisticas de $Luchador1_fuerza, $Luchador1_resistencia y Luchador1_destreza y $Luchador2_nombre de tipo $Luchador2_tipo con estadisticas de $Luchador2_fuerza, $Luchador2_resistencia y Luchador2_destreza")
-    public void PresentarLuchadores(int Luchador1_tipo, int Luchador1_fuerza, int Luchador1_resistencia, int Luchador1_destreza, int Luchador1_puntuacion, int Luchador2_tipo, int Luchador2_fuerza, int Luchador2_resistencia, int Luchador2_destreza, int Luchador2_puntuacion) {
+    public void PresentarLuchadores(String Luchador1_nombre, int Luchador1_tipo, int Luchador1_fuerza, int Luchador1_resistencia, int Luchador1_destreza, String Luchador2_nombre, int Luchador2_tipo, int Luchador2_fuerza, int Luchador2_resistencia, int Luchador2_destreza) {
         if (Luchador1_tipo==1) {
             Luchador1_puntuacion = 2 * Luchador1_fuerza + Luchador1_resistencia;
         } else if (Luchador1_tipo==2) {
