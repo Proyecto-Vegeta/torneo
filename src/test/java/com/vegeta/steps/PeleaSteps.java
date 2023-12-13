b469e74d08831629b3d2aa10d5699ae22fefa829
LeoTheEvil <iceninjashidomaru@gmail.com>
2023-12-13 12:55:49 -0300

    Test Automation

--- src/test/java/com/vegeta/steps/PeleaSteps.java
+++ src/test/java/com/vegeta/steps/PeleaSteps.java
@@ -8,13 +8,13 @@ import org.junit.Assert;
 public class PeleaSteps {
 
     String Luchador1_nombre;
-    int Luchador1_tipo=1;
+    int Luchador1_tipo;
     int Luchador1_fuerza;
     int Luchador1_resistencia;
     int Luchador1_destreza;
     int Luchador1_puntuacion;
     String Luchador2_nombre;
-    int Luchador2_tipo=1;
+    int Luchador2_tipo;
     int Luchador2_fuerza;
     int Luchador2_resistencia;
     int Luchador2_destreza;
@@ -22,7 +22,7 @@ public class PeleaSteps {
     String Ganador;
 
     @Given("$Luchador1_nombre de tipo $Luchador1_tipo con estadisticas de $Luchador1_fuerza, $Luchador1_resistencia y Luchador1_destreza y $Luchador2_nombre de tipo $Luchador2_tipo con estadisticas de $Luchador2_fuerza, $Luchador2_resistencia y Luchador2_destreza")
-    public void PresentarLuchadores() {
+    public void PresentarLuchadores(int Luchador1_tipo, int Luchador1_fuerza, int Luchador1_resistencia, int Luchador1_destreza, int Luchador1_puntuacion, int Luchador2_tipo, int Luchador2_fuerza, int Luchador2_resistencia, int Luchador2_destreza, int Luchador2_puntuacion) {
         if (Luchador1_tipo==1) {
             Luchador1_puntuacion = 2 * Luchador1_fuerza + Luchador1_resistencia;
         } else if (Luchador1_tipo==2) {
@@ -39,7 +39,7 @@ public class PeleaSteps {
         }
     }
     @When("$Luchador1_nombre pelea contra $Luchador2_nombre")
-    public void LuchadoresPelean() {
+    public void LuchadoresPelean(String Luchador1_nombre, int Luchador1_puntuacion, String Luchador2_nombre, int Luchador2_puntuacion) {
         if (Luchador1_puntuacion>Luchador2_puntuacion) {
             Ganador = Luchador1_nombre;
         } else if (Luchador1_puntuacion<Luchador2_puntuacion) {
